c6f0f820a73f3b4654cecf4c6220a1f2b5fc8a50
darusam@cdmsmith.com <darusam@cdmsmith.com>
2014-09-28 16:16:56 -0400

    initialize project

---
+++ CDMSmith.OpenRasta.Legacy/Codecs/JsonCodec.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using OpenRasta.Codecs;
+using OpenRasta.Web;
+using OpenRasta.TypeSystem;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CDMSmith.OpenRasta.Legacy.Codecs
+{
+    [MediaType("application/json", "json")]
+    public class JsonCodec : IMediaTypeReader, IMediaTypeWriter
+    {
+        public object Configuration { get; set; }
+
+        
+        public object ReadFrom(IHttpEntity request, IType destinationType, string destinationName)
+        {
+            if (destinationType.StaticType == null)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                string toConvert = string.Empty;
+                using(System.IO.StreamReader reader = new System.IO.StreamReader(request.Stream))
+                {
+                    toConvert = reader.ReadToEnd();
+                }
+                object toReturn = Newtonsoft.Json.JsonConvert.DeserializeObject(toConvert, destinationType.StaticType);
+                return toReturn;
+            }
+        }
+
+        public void WriteTo(object entity, IHttpEntity response, string[] codecParameters)
+        {
+            if (entity == null)
+            {
+                return;
+            }
+            else
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Converters.Add(new JavaScriptDateTimeConverter());
+                serializer.NullValueHandling = NullValueHandling.Ignore;
+
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(response.Stream))
+                {
+                    using (JsonWriter jw = new JsonTextWriter(sw))
+                    {
+                        serializer.Serialize(jw, entity);
+                        jw.Flush();
+                        jw.Close();
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ CDMSmith.OpenRasta.Legacy/Configuration/JsonCodecConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenRasta.Configuration.Fluent;
+
+namespace CDMSmith.OpenRasta.Legacy.Configuration
+{
+    public static class JsonCodecConfigurationExtensions
+    {
+        public static ICodecDefinition AsJsonData(this ICodecParentDefinition codecParent)
+        {
+            return codecParent.TranscodedBy<CDMSmith.OpenRasta.Legacy.Codecs.JsonCodec>(null);
+        }
+    }
+}
